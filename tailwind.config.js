1041efcdf1060be608eb08ecc2806da1fabaccee
Dev <dev@franznkemaka.com>
2024-04-02 12:41:44 +0200

    initial react native base

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}",
+					  "./app/routes/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
