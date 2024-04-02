8e15d0be2a93a73d9f8e724af560194ffa945c0d
Dev <dev@franznkemaka.com>
2024-04-02 13:14:13 +0200

    initial files

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import WelcomePage from './routes/WelcomePage'
+
+export default function index() {
+  return (
+    <WelcomePage/>
+  )
+}
