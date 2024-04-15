f361598db155f6893c2131c738b356189457e924
Cryptocryxx <143177997+Cryptocryxx@users.noreply.github.com>
2024-04-15 12:45:52 +0200

    clarification for backend

--- backend/controllers/controllerRegister.js
+++ backend/controllers/controllerRegister.js
@@ -5,9 +5,15 @@ async function postRegister(req, res) {
     const pushingUser = await database.postUser(req)
     if(pushingUser == "Success!"){
         res.json(pushingUser)
-    }else if(pushingUser == "Duplicate username" || pushingUser == "Duplicate Email" || pushingUser == "Email format false" || pushingUser == "Password format false"){
+    }else if(pushingUser == "Duplicate username") {
         res.status(400).json(pushingUser)
-    }else{
+    }else if (pushingUser == "Duplicate Email"){
+        res.status(401).json(pushingUser)
+    }else if (pushingUser == "Email format false"){
+        res.status(402).json(pushingUser)
+    }else if (pushingUser == "Password format false"){
+        res.status(403).json(pushingUser)
+    }else {
         res.status(404).json(pushingUser)
     }
 }
