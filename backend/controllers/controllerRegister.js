90f152240bf374fd5165e41dd39ca40139aea178
Joel Starkov <js486@hdm-stuttgart.de>
2024-04-15 11:55:30 +0200

    commit for merge request

--- backend/controllers/controllerRegister.js
+++ backend/controllers/controllerRegister.js
@@ -5,6 +5,8 @@ async function postRegister(req, res) {
     const pushingUser = await database.postUser(req)
     if(pushingUser == "Success!"){
         res.json(pushingUser)
+    }else if(pushingUser == "Duplicate username" || pushingUser == "Duplicate Email" || pushingUser == "Email format false" || pushingUser == "Password format false"){
+        res.status(400).json(pushingUser)
     }else{
         res.status(404).json(pushingUser)
     }
@@ -13,7 +15,7 @@ async function postRegister(req, res) {
 async function deleteRegister(req, res) {
 
          const deleteUser = await database.deleteUser(req)
-
+        console.log(deleteUser)
         if(deleteUser == "Benutzer erfolgreich gelöscht"){
             res.json(deleteUser)
         }else{
