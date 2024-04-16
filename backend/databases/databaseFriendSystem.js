46b336dab6d31d902963c8d64987e43d35aad1c0
Cryptocryxx <143177997+Cryptocryxx@users.noreply.github.com>
2024-04-16 02:41:31 +0200

    ref #10

--- backend/databases/databaseFriendSystem.js
+++ backend/databases/databaseFriendSystem.js
@@ -172,7 +172,7 @@ async function getFriendRecommendations(req) {
     return friendRecommendations;
 }
 
-/*async function getInvitations(req) {
+async function getInvitations(req) {
     try {
         const username = req.params.username;
         const personalInformation = await database.getDB().collection("personalInformation");
@@ -191,7 +191,7 @@ async function getFriendRecommendations(req) {
     }catch (err) {
 
     }
-}*/
+}
 
 module.exports = {
     postFriendRequest,
@@ -201,5 +201,5 @@ module.exports = {
     //getFriendNameList,
     getFriendList,
     getFriendRecommendations,
-    //getInvitations
+    getInvitations
 }
