20128872b57195a7767453b5605b9141e6d5d596
Lars Gerigk <dev@franznkemaka.com>
2024-05-08 15:34:47 +0200

    Fixing Error #22

--- frontend/app/routes/HomePage.js
+++ frontend/app/routes/HomePage.js
@@ -43,20 +43,28 @@ export default function HomePage({ user, displayFriend, handleFriendSelection })
                         {/* Content ScrollView */}
                         <ScrollView showsHorizontalScrollIndicator={false} contentContainerStyle={{ flexGrow: 1, paddingBottom: 200 }}>
                             {
-                                user.friends.map((friend, index) => (
-                                    [
-                                        <HomeActivityCard friend={friend} key={`friend-${index}`} />,
+                                user.friends.map((friend, index) => {
+                                    return (
+                                        <View key={index}>
+                                            <HomeActivityCard friend={friend}/>
 
-                                        // Separation line
-                                        < View className = { classNames('w-full h-[2px] mt-4 bg-secondary') } />,
-                                        // Füge die EventInfoCard und Events abwechselnd ein
-                                        index % 2 ?
-                                            <Events onSelectEvent={() => { }} selectedEvent={events[index]} key={`events-${index}`} />
-                                            : <EventInfoCard event={events[index]} key={`event-info-${index}`} />,
-                                        // Füge die Separation Line hinzu, außer beim letzten Element
-                                        index !== user.friends.length - 1 && <View className={classNames('w-full h-[2px] mt-4 bg-secondary')} key={`separator-${index}`} />
-                                    ]
-                                ))
+                                            {/* Separation line */}
+                                            < View className={classNames('w-full h-[2px] mt-4 bg-secondary')} />
+
+                                            {/* Füge die EventInfoCard und Events abwechselnd ein */}
+                                            {index % 2 ?
+                                                <Events onSelectEvent={() => { }} selectedEvent={events[index]}/>
+                                                : <EventInfoCard event={events[index]}/>
+                                            }
+
+                                            {/* Füge die Separation Line hinzu, außer beim letzten Element */}
+                                            {index !== user.friends.length - 1 &&
+                                                <View className={classNames('w-full h-[2px] mt-4 bg-secondary')}/>
+                                            }
+                                        </View>
+                                    )
+                                }
+                                )
                             }
                         </ScrollView>
                     </>
