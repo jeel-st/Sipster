5767f0cdbe71ed51d68c0641df26599f1f41c47a
mk414 <mk414@hdm-stuttgart.de>
2024-05-15 11:15:29 +0200

    Games werden in zwei Zeilen angezeigt

--- frontend/app/routes/index.js
+++ frontend/app/routes/index.js
@@ -3,6 +3,7 @@ import EventPage from "./EventPage";
 import HomePage from "./HomePage";
 import LoginPage from "./LoginPage";
 import RegisterPage from "./RegisterPage";
+import FriendsPage from "./FriendsPage"
 import GamePageAll from "./GamePageAll";
 import { withHook } from "../utils/withHook";
 import useHome from "../utils/hooks/useHome";
@@ -14,4 +15,4 @@ const Home = withHook(useHome, HomePage)
 const Friends = withHook(useFriends, FriendsPage)
 
 
-export { GamePage, EventPage, Home, LoginPage, RegisterPage, GamePageAll, Friends, AccountPage, SettingsPage }
+export { GamePage, EventPage, Home, LoginPage, RegisterPage, FriendsPage, GamePageAll, Friends, AccountPage, SettingsPage }
