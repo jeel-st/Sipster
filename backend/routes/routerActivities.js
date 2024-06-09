73dbb0cf536564e4ec7c1823648d203f42e350b3
Cryptocryxx <143177997+Cryptocryxx@users.noreply.github.com>
2024-06-10 01:34:29 +0200

    switched post to put ref #35

--- backend/routes/routerActivities.js
+++ backend/routes/routerActivities.js
@@ -7,7 +7,7 @@ router.use(logMiddleware);
 const activitiesController = require('../controllers/controllerActivities')
 
 router.post('/postActivity', activitiesController.postActivity)
-router.post('/addReaction', activitiesController.addReaction)
+router.put('/addReaction', activitiesController.addReaction)
 router.get('/:userID', activitiesController.getActivities)
 router.put('/postBeforePicture', activitiesController.postBeforePicture)
 router.put('/postAfterPicture', activitiesController.postAfterPicture)
