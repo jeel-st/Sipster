acb9715283a11d11e26d5ea35d1d3a2628e18afc
jstarkov <js486@hdm-stuttgart.de>
2024-06-19 19:36:18 +0200

    new tests Ref #55

--- backend/tests/JestTests/login.test.js
+++ backend/tests/JestTests/login.test.js
@@ -5,7 +5,14 @@ const baseUrl = 'http://85.215.71.124';
 test('successful login', async () => {
     const username = 'Sipster';
     const password = 'JoelStinkt*100';
-    const response = await axios.get(`${baseUrl}/login/${username}/${password}`);
+    const response = await axios.post(`${baseUrl}/login`,{
+        "username": `${username}`,
+        "password": `${password}`
+    },{
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
     expect(response.status).toBe(200);
     expect(response.data).toBe(true);
 });
@@ -14,7 +21,14 @@ test('user not found', async () => {
     const username = 'gamsa';
     const password = 'Ansaf86!!mm';
     try {
-        await axios.get(`${baseUrl}/login/${username}/${password}`);
+        await axios.post(`${baseUrl}/login`,{
+            "username": `${username}`,
+            "password": `${password}`
+        },{
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
     } catch (error) {
         expect(error.response.status).toBe(401);
         expect(error.response.data).toBe('User not found');
