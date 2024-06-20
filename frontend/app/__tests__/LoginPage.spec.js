26c681f57feff65886ac9a9f161b0b13e6d2b861
mk414 <mk414@hdm-stuttgart.de>
2024-06-20 12:45:25 +0200

    #54 Login render Test

--- frontend/app/__tests__/LoginPage.spec.js
+++ frontend/app/__tests__/LoginPage.spec.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
 import LoginPage from '../routes/LoginPage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { render } from '@testing-library/react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+// Mocke die Navigation für den Test
+jest.mock('@react-navigation/native', () => ({
+  ...jest.requireActual('@react-navigation/native'),
+  useNavigation: () => ({
+    navigate: jest.fn(),
+  }),
+}));
+// Mocke AsyncStorage mit jest.mock
+jest.mock('@react-native-async-storage/async-storage', () => ({
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  // Füge hier weitere benötigte Methoden hinzu
+}));
 
 describe('LoginPage Component', () => {
+  
   it('renders correctly', () => {
-    const { getByPlaceholderText, getByText } = render(<LoginPage />);
+    const { getByPlaceholderText, getByText } = render(<NavigationContainer>
+      <LoginPage />
+    </NavigationContainer>
+    );
 
     // Check if the input fields are rendered
     expect(getByPlaceholderText('username')).toBeTruthy();
