3b052100856bccd3c520cbb3c5caf60e15fd5c54
Cryptocryxx <143177997+Cryptocryxx@users.noreply.github.com>
2024-06-25 00:46:43 +0200

    Updated Hompage Backend.test.js ref #55

--- backend/tests/JestTests/homepage.test.js
+++ backend/tests/JestTests/homepage.test.js
@@ -1,29 +1,61 @@
-const axios = require("axios")
+const axios = require("axios");
 
 const baseUrl = 'http://85.215.71.124';
 
-test('getting Homepage for the first Time', async () => {
-    const userID = "663bd3b7969fc6302facf1ee"
-    const response = await axios.put(`${baseUrl}/homepage`,{
-        "userID": userID,
-        "usedIDs": []
-    },{
-        headers: {
-            'Content-Type': 'application/json'
+test('Fetching Homepage and validationg data and checking excluded ids', async () => {
+    const usedIDs = [];
+    for (let limit = 0; limit < 10; limit++) {
+        const homepage = await getHomepageTest(usedIDs);
+        for (const element of homepage) {
+            usedIDs.push({
+                type: element.type,
+                id: element._id
+            });
         }
-    });
+    }
+});
+
+
+async function getHomepageTest(usedIDs) {
+    const userID = "663bd3b7969fc6302facf1ee";
     try {
+        const response = await axios.put(`${baseUrl}/homepage`, {
+            "userID": userID,
+            "usedIDs": usedIDs
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
         for (const element of response.data) {
             expect(response.status).toBe(200);
             expect(element).toHaveProperty('_id');
-            switch (element.type){
-                case "game": expect(element).toHaveProperty('playtime'); break;
-                case "event": expect(element).toHaveProperty('header'); break;
-                case "activtiy": expect(element).toHaveProperty('playtime'); break;
-                default: expect(response.data[0]).toHaveProperty('TestFailed'); break; //if the default value is reached the Test Failed!
+            switch (element.type) {
+                case "game":
+                    expect(element).toHaveProperty('playtime');
+                    testUsedIds(element, usedIDs);
+                    break;
+                case "event":
+                    expect(element).toHaveProperty('header');
+                    testUsedIds(element, usedIDs);
+                    break;
+                case "activity":
+                    expect(element).toHaveProperty('reactions');
+                    testUsedIds(element, usedIDs);
+                    break;
+                default:
+                    throw new Error("Test failed!"); //if the default value is reached the Test Failed!
             }
         }
+        return response.data;
     } catch (err) {
-        console.log(err)
+        console.log(err);
     }
-});
+}
+
+function testUsedIds(element, usedIDs) {
+    for (const usedID of usedIDs) {
+            expect(element._id).not.toBe(usedID.id);
+    }
+}
